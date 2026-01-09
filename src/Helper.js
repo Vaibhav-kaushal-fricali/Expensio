fc7e3adaeb07db0cab7478a33caab65910a89944
Vaibhav Kaushal <vaibhav.kaushal@planetspark.in>
2026-01-09 15:36:21 +0530

    helper to use global variable

--- src/Helper.js
+++ src/Helper.js
@@ -1,2 +1,14 @@
 import { CATEGORIES } from "./Constant";
 
+export const getID = () => {
+  return Date.now().toString() + Math.floor(Math.random() * 1000).toString();
+};
+
+export const getDate = () => {
+  return new Date().toISOString().split("T")[0];
+};
+
+export const getCategoryColor = (color) => {
+  const category = CATEGORIES.find((cat) => cat.color === color);
+  return category ? category.color : "#808080";
+};
